9e92c9b50bb678c4850c9a7487f0499f2aa42841
Simon McManis <43659281+smcmanis@users.noreply.github.com>
2022-04-08 23:18:49 -0700

    Entrypoint to set a custom timescale mechanic

---
+++ SuperhotVR_EEG/OverrideGlobalTimescale.cs
@@ -0,0 +1,25 @@
+﻿using HarmonyLib;
+using MelonLoader;
+
+namespace SuperhotVR_EEG
+{ 
+    [HarmonyPatch(typeof(TimeControl), "GetAggregateTargetTimescale")]
+    static class TimeControl_GetAggregateTargetTimescale_Patch
+    {
+        static bool Prefix(ref float __result)
+        {
+            /**
+             *  GetAggregateTargetTimescale
+             *  This method is called onUpdate to set the current world timescale. 
+             *  The original method takes the sum of the CurrentTimeModifier values of every
+             *  in the TimeControl TimeShifterRegistry. There are TimeShifters for the HMD, 
+             *  leftHandControllor, and rightHandControllor, weapons, etc. 
+             *  
+             *  Here, we can inject are own timescale value, such as one calculated for a 
+             *  PlayerFocusLevel etc.
+            **/
+            __result = 0.2f; 
+            return false; //don't let the original method run
+        }
+    }
+}
