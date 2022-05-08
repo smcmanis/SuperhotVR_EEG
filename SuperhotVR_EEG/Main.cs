55311a3528af62eee66e0cb3c1d105f768b45177
Simon McManis <43659281+smcmanis@users.noreply.github.com>
2022-05-08 01:24:23 -0700

    Refactor a bit cleaner

--- SuperhotVR_EEG/Main.cs
+++ SuperhotVR_EEG/Main.cs
@@ -11,8 +11,12 @@ namespace SuperhotVR_EEG
 {
     public class Main : MelonMod
     {
-        public static Queue<float> queue = new Queue<float>();
+        public static Queue<float> focusHistory = new Queue<float>();
         public static WebSocketServer wss = new WebSocketServer(11111);
+        public static float DEFAULT_TIMESCALE = 0.3f;
+        public static float currentFocus = 0f;
+        public static float currentTimescale = DEFAULT_TIMESCALE;
+        public static bool useFocusTimescale = true;
 
         public class Focus : WebSocketBehavior
         {
@@ -23,31 +27,81 @@ namespace SuperhotVR_EEG
                     float focus = float.Parse(e.Data);
                     if (focus > 0)
                     { 
-                        queue.Enqueue(focus);
-                        if (queue.Count > 5)
+                        focusHistory.Enqueue(focus);
+                        if (focusHistory.Count > 5)
                         {
-                            queue.Dequeue();
+                            focusHistory.Dequeue();
                         }
+                        currentFocus = averageFocus();
                     }
                 }
             }
         }
+
+        public static float averageFocus()
+        {
+            float focus = Queryable.Average(focusHistory.AsQueryable());
+            MelonLogger.Msg("focus avg: " + focus);
+            return focus;
+        }
+
+        public static float timescaleFromFocus(float focus)
+        {
+            float timescale = 0f;
+            if (focus < 0.1f)
+            {
+                timescale = 1f;
+            } 
+            else if (focus < 0.2f)
+            {
+                timescale = 0.5f;
+            }
+            else if (focus < 0.25f)
+            {
+                timescale = 0.4f;
+            }
+            else if (focus < 0.3f)
+            {
+                timescale = 0.3f;
+            }
+            else if (focus < 0.5f)
+            {
+                timescale = 0.2f;
+            }
+            else if (focus < 0.6f)
+            {
+                timescale = 0.1f;
+            }
+            else if (focus < 0.7f)
+            {
+                timescale = 0.05f;
+            }
+            else
+            {
+                timescale = 0f;
+            }
+
+            return timescale;
+        }
+
         public override void OnApplicationStart()
         {
             wss.AddWebSocketService<Focus>("/Focus");
             wss.Start();
         }
+
         public override void OnApplicationQuit()
         {
             wss.Stop();
         }
+
         public override void OnUpdate()
         {
             
             if (Input.GetKeyDown(KeyCode.G))
             {
-                MelonLogger.Msg("You just pressed G");
- 
+                useFocusTimescale = !useFocusTimescale;
+                MelonLogger.Msg("Focus Timescale is: " + useFocusTimescale);
             }
         }
 
@@ -55,35 +109,6 @@ namespace SuperhotVR_EEG
         [HarmonyPatch(typeof(TimeControl), "GetAggregateTargetTimescale")]
         static class TimeControl_GetAggregateTargetTimescale_Patch
         {
-            //public async static void getFocus()
-            //{
-                
-           //    // if this works, i'll have to use a queue or something to store the async focus values
-            //    // then pull from queue
-            //    // if queue empty, use last value or something
-            //    // Just need some way to handle the async stuff
-            //    float focus = 0.1f;
-            //    try
-            //    {
-            //        HttpResponseMessage response = await client.GetAsync("http://192.168.0.13:8000");
-            //        response.EnsureSuccessStatusCode();
-            //        string responseBody = await response.Content.ReadAsStringAsync();
-            //        MelonLogger.Msg(responseBody);
-
-            //    }
-            //    catch (HttpRequestException ex)
-            //    {
-            //        MelonLogger.Msg("\nException Caught!");
-            //        MelonLogger.Msg("Message :{0} ", ex.Message);
-            //    }
-            //    queue.Enqueue(focus);
-            //}
-
-            //private static readonly HttpClient client = new HttpClient();
-
-            
-            
-
             static bool Prefix(ref float __result)
             {
                 /**
@@ -96,37 +121,15 @@ namespace SuperhotVR_EEG
                  *  Here, we can inject are own timescale value, such as one calculated for a 
                  *  PlayerFocusLevel etc.
                 **/
-                
-                // IDEA = queue should have length of 10 (3 seconds?) and then i can take the average
 
-                if (queue.Count > 0)
+                if (useFocusTimescale)
                 {
-                    // reveresed for testing it works
-              
-                    if (queue.Count > 5)
-                    {
-
-                        queue.Dequeue();
-                    }
-                    float focus = Queryable.Average(queue.AsQueryable());
-                    MelonLogger.Msg("focus avg: " + focus + "(N = "+queue.Count+ ")");
-                    if (focus < 0.1f) {
-                        __result = 0.4f;
-                    } else if (focus < 0.2) 
-                    {
-                        __result = 0.3f;
-                    } else if (focus < 0.5)
-                    {
-                        __result = 0.1f;
-                    } else 
-                    {
-                        __result = 0.05f;
-                    }
+                    __result = timescaleFromFocus(currentFocus);
+                    return false;
                 } else
                 {
-                    __result = 0f; 
+                    return true;
                 }
-                return false; //don't let the original method run
             }
         }
     }
