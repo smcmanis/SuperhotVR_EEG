85869142baab83006da2b47905dcf2b716b1ae34
Simon McManis <43659281+smcmanis@users.noreply.github.com>
2022-05-07 17:23:52 -0700

    Queue with values from web socket read by patch

--- SuperhotVR_EEG/Main.cs
+++ SuperhotVR_EEG/Main.cs
@@ -3,14 +3,44 @@ using UnityEngine;
 using HarmonyLib;
 using System.Threading.Tasks;
 using System.Collections.Generic;
-using UnityEngine.Networking;
-using System.Collections;
+using WebSocketSharp;
+using WebSocketSharp.Server;
+using System.Linq;
 
 namespace SuperhotVR_EEG
 {
     public class Main : MelonMod
     {
+        public static Queue<float> queue = new Queue<float>();
+        public static WebSocketServer wss = new WebSocketServer(11111);
 
+        public class Focus : WebSocketBehavior
+        {
+            protected override void OnMessage(MessageEventArgs e)
+            {
+                if (e.Data != null)
+                {
+                    float focus = float.Parse(e.Data);
+                    if (focus > 0)
+                    { 
+                        queue.Enqueue(focus);
+                        if (queue.Count > 5)
+                        {
+                            queue.Dequeue();
+                        }
+                    }
+                }
+            }
+        }
+        public override void OnApplicationStart()
+        {
+            wss.AddWebSocketService<Focus>("/Focus");
+            wss.Start();
+        }
+        public override void OnApplicationQuit()
+        {
+            wss.Stop();
+        }
         public override void OnUpdate()
         {
             
@@ -25,10 +55,9 @@ namespace SuperhotVR_EEG
         [HarmonyPatch(typeof(TimeControl), "GetAggregateTargetTimescale")]
         static class TimeControl_GetAggregateTargetTimescale_Patch
         {
-            public async static void getFocus(float currentFocus)
-            {
-
-                queue.Enqueue(currentFocus + 0.1f);
+            //public async static void getFocus()
+            //{
+                
            //    // if this works, i'll have to use a queue or something to store the async focus values
             //    // then pull from queue
             //    // if queue empty, use last value or something
@@ -48,13 +77,12 @@ namespace SuperhotVR_EEG
             //        MelonLogger.Msg("Message :{0} ", ex.Message);
             //    }
             //    queue.Enqueue(focus);
-            }
+            //}
 
             //private static readonly HttpClient client = new HttpClient();
 
             
-        
-            private static Queue<float> queue = new Queue<float>();
+            
 
             static bool Prefix(ref float __result)
             {
@@ -69,15 +97,35 @@ namespace SuperhotVR_EEG
                  *  PlayerFocusLevel etc.
                 **/
                 
+                // IDEA = queue should have length of 10 (3 seconds?) and then i can take the average
+
                 if (queue.Count > 0)
                 {
-                    __result = queue.Dequeue();
+                    // reveresed for testing it works
+              
+                    if (queue.Count > 5)
+                    {
+
+                        queue.Dequeue();
+                    }
+                    float focus = Queryable.Average(queue.AsQueryable());
+                    MelonLogger.Msg("focus avg: " + focus + "(N = "+queue.Count+ ")");
+                    if (focus < 0.1f) {
+                        __result = 0.4f;
+                    } else if (focus < 0.2) 
+                    {
+                        __result = 0.3f;
+                    } else if (focus < 0.5)
+                    {
+                        __result = 0.1f;
+                    } else 
+                    {
+                        __result = 0.05f;
+                    }
                 } else
                 {
-                    __result = 0f;
+                    __result = 0f; 
                 }
-                getFocus(__result);
-                MelonLogger.Msg(__result);
                 return false; //don't let the original method run
             }
         }
